83914c656ab47b2d8604f246865f8845702195b2
Xbat <99105387+Xbatovec@users.noreply.github.com>
2023-08-17 00:57:28 +0200

    Preparation

--- src/script/index.js
+++ src/script/index.js
@@ -1,30 +1,41 @@
 var Player = /** @class */ (function () {
-    function Player(length, pos, speed) {
+    function Player(length, pos, speed, direction) {
         this.length = length;
         this.pos = pos;
         this.speed = speed;
+        this.direction = direction;
     }
     Player.prototype.getLength = function () {
         return this.length;
     };
-    Player.prototype.getPos = function () {
-        return this.pos;
-    };
-    Player.prototype.getSpeed = function () {
-        return this.speed;
-    };
     Player.prototype.setLength = function (length) {
         this.length = length;
+    };
+    Player.prototype.getPos = function () {
+        return this.pos;
     };
     Player.prototype.setPos = function (pos) {
         this.pos = pos;
     };
+    Player.prototype.getSpeed = function () {
+        return this.speed;
+    };
     Player.prototype.setSpeed = function (speed) {
         this.speed = speed;
     };
+    Player.prototype.getDirection = function () {
+        return this.direction;
+    };
+    Player.prototype.setDirection = function (direction) {
+        this.direction = direction;
+    };
     return Player;
 }());
-var player = new Player(1, { x: 6, y: 6 }, 1);
-console.log('Hello!', player.getLength());
-player.setLength(32);
-console.log('Hello!', player.getLength());
+var player = new Player(1, { x: 6, y: 6 }, 1, 'down');
+function loop() {
+    var length = player.getLength();
+    var pos = player.getPos();
+    var speed = player.getSpeed();
+    var direction = player.getDirection();
+    setTimeout(function () { return loop(); }, 1000);
+}
--- src/script/index.ts
+++ src/script/index.ts
@@ -2,40 +2,56 @@ class Player {
     private length: number;
     private pos: object;
     private speed: number;
+    private direction: string;
 
-    public constructor(length: number, pos: object, speed: number) {
+    public constructor(length: number, pos: object, speed: number, direction: string) {
         this.length = length;
         this.pos = pos;
         this.speed = speed;
+        this.direction = direction;
     }
 
     public getLength(): number {
         return this.length;
     }
 
-    public getPos(): object {
-        return this.pos;
-    }
-
-    public getSpeed(): number {
-        return this.speed;
-    }
-
     public setLength(length: number): void {
         this.length = length;
+    }
+
+    public getPos(): object {
+        return this.pos;
     }
 
     public setPos(pos: object): void {
         this.pos = pos;
     }
 
+    public getSpeed(): number {
+        return this.speed;
+    }
+
     public setSpeed(speed: number): void {
         this.speed = speed;
     }
+
+    public getDirection(): string {
+        return this.direction;
+    }
+
+    public setDirection(direction: string): void {
+        this.direction = direction;
+    }
 }
 
-const player = new Player(1, {x: 6, y: 6}, 1);
+const player = new Player(1, {x: 6, y: 6}, 1, 'down');
 
-console.log('Hello!', player.getLength());
-player.setLength(32);
-console.log('Hello!', player.getLength());
+function loop() {
+
+    let length = player.getLength();
+    let pos = player.getPos();
+    let speed = player.getSpeed();
+    const direction = player.getDirection();
+
+    setTimeout(() => loop(), 1000);
+}
